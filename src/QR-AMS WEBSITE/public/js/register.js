c0d384db4a8735e6cece53789b82547b6368fe3f
Spooky <gianepanto@gmail.com>
2023-05-13 23:22:02 +0800

    added register page, have not linked with backend

---
+++ src/QR-AMS WEBSITE/public/js/register.js
@@ -0,0 +1,32 @@
+const gradeLevelSelector = document.getElementById('gradeLevel')
+const studentName = document.getElementById('studentName');
+const registerButton = document.getElementById('registerButton');
+
+let isGradeSelected = false
+let isSectionSelected = false
+
+gradeLevelSelector.addEventListener('change', () => {
+  isGradeSelected = true
+  const currentSection = document.getElementById(gradeLevelSelector.value)
+  const currentSectionVisibility = currentSection.getAttribute('data-visible')
+
+  const allSections = document.querySelectorAll('.sections');
+  allSections.forEach(section => section.setAttribute('data-visible', 'false'));
+
+  if (currentSectionVisibility === 'false') {
+    isSectionSelected = true
+    currentSection.setAttribute('data-visible', 'true')
+  }
+
+  if (isGradeSelected && isSectionSelected) {
+    studentName.addEventListener('input', () => {
+      if (studentName.value !== '') {
+        registerButton.disabled = false;
+      }
+    });
+    
+    registerButton.addEventListener('click', () => {
+      console.log(`${studentName.value} | ${gradeLevelSelector.value} | ${currentSection.value}`);
+    });
+  }
+})
